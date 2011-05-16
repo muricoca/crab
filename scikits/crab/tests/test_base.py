fe83ccb54cd4e08fd9315681968f025ae82fe4b5
Marcel Caraciolo <caraciol@gmail.com>
2011-05-16 17:37:41 -0300

    Added the Base Recommender Class.

---
+++ scikits/crab/tests/test_base.py
@@ -0,0 +1,34 @@
+#-*- coding:utf-8 -*-
+
+""" 
+Base Recommender Models.
+"""
+
+# Authors: Marcel Caraciolo <marcel@muricoca.com>
+#          Bruno Melo <bruno@muricoca.com>
+# License: BSD Style.
+
+import unittest
+import sys
+
+sys.path.append('/Users/marcelcaraciolo/Desktop/Orygens/crab/crab/scikits/craba')
+
+from base import BaseRecommender
+
+#test classes
+
+class MyRecommender(BaseRecommender):
+    def __init__(self,model):
+        BaseRecommender.__init__(self,model)
+    
+################################################################################
+# The tests
+
+
+class testBaseRecommender(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
+
