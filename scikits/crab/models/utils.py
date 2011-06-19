c0e6cab9faa6c4631885e51907eb232d2bf754ec
Douglas Soares de Andrade <contato@douglasandrade.com>
2011-06-19 14:03:23 -0300

    Small pep8 fixes

--- scikits/crab/models/utils.py
+++ scikits/crab/models/utils.py
@@ -1,6 +1,6 @@
 #-*- coding:utf-8 -*-
 
-""" 
+"""
 Utilities functions and classes for models.
 
 """
@@ -10,6 +10,7 @@ Utilities functions and classes for models.
 
 class UserNotFoundError(Exception):
     pass
+
 
 class ItemNotFoundError(Exception):
     pass
