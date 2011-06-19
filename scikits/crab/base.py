3f7118f3a8d9ae54d4bc3ec26407ce5394f1a37b
Douglas Soares de Andrade <contato@douglasandrade.com>
2011-06-19 12:54:41 -0300

    Removing unused numpy import and some pep8 fixes

--- scikits/crab/base.py
+++ scikits/crab/base.py
@@ -1,6 +1,6 @@
 #-*- coding:utf-8 -*-
 
-""" 
+"""
 Base Recommender Models.
 """
 
@@ -8,55 +8,56 @@ Base Recommender Models.
 #          Bruno Melo <bruno@muricoca.com>
 # License: BSD Style.
 
-
-import numpy as np
-
 from scikits.learn.base import BaseEstimator
+
 
 class BaseRecommender(BaseEstimator):
     """
     Base Class for Recommenders that suggest items for users.
-    
+
     Should not be used directly, use derived classes instead
 
     Attributes
     ----------
 
-     `model`:  DataModel  
+     `model`:  DataModel
           Defines the data model where data is fetched.
 
     """
-    def __init__(self,model):
+
+    def __init__(self, model):
         self.model = model
-    
-    def recommend(self,user_id,how_many,**params):
+
+    def recommend(self, user_id, how_many, **params):
         '''
-        Return a list of recommended items, ordered from most strongly recommend to least.
-         
+        Return a list of recommended items, ordered from most strongly
+        recommend to least.
+
         Parameters
         ----------
         user_id: int or string
                  User for which recommendations are to be computed.
-        how_many: int 
+        how_many: int
                  Desired number of recommendations
         rescorer:  function, optional
-                 Rescoring function to apply before final list of recommendations.
+                 Rescoring function to apply before final list of
+                 recommendations.
 
         '''
         raise NotImplementedError("BaseRecommender is an abstract class.")
-                
-    def estimate_preference(self,**params):
+
+    def estimate_preference(self, **params):
         '''
-        Return an estimated preference if the user has not expressed a preference for the item, or else the 
-        user's actual preference for the item. If a preference cannot be estimated, returns None.
-        
+        Return an estimated preference if the user has not expressed a
+        preference for the item, or else the user's actual preference for the
+        item. If a preference cannot be estimated, returns None.
         '''
         raise NotImplementedError("BaseRecommender is an abstract class.")
 
-    def all_other_items(self,user_id):
+    def all_other_items(self, user_id):
         '''
-        Return all items in the `model` for which the user has not expressed the preference and
-        could possibly be recommended to the user.
+        Return all items in the `model` for which the user has not expressed
+        the preference and could possibly be recommended to the user.
 
         Parameters
         ----------
@@ -64,10 +65,11 @@ class BaseRecommender(BaseEstimator):
                  User for which recommendations are to be computed.
         '''
         raise NotImplementedError("BaseRecommender is an abstract class.")
-        
-    def set_preference(self,user_id, item_id, value):
+
+    def set_preference(self, user_id, item_id, value):
         '''
-        Set a new preference of a user for a specific item with a certain magnitude.
+        Set a new preference of a user for a specific item with a certain
+        magnitude.
 
         Parameters
         ----------
@@ -77,12 +79,13 @@ class BaseRecommender(BaseEstimator):
         item_id: int or string
                  Item that will be updated.
 
-        value:  The new magnitude for the preference of a item_id from a user_id. 
+        value:  The new magnitude for the preference of a item_id from a
+                user_id.
 
         '''
-        self.model.set_preference(user_id,item_id,value)
-                
-    def remove_preference(self,user_id, item_id):
+        self.model.set_preference(user_id, item_id, value)
+
+    def remove_preference(self, user_id, item_id):
         '''
         Remove a preference of a user for a specific item
 
@@ -94,4 +97,4 @@ class BaseRecommender(BaseEstimator):
                  Item that will be removed the preference for the user_id.
 
         '''
-        self.model.remove_preference(user_id,item_id)
+        self.model.remove_preference(user_id, item_id)
