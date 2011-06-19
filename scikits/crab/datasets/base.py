a25054045a8f2f8fb03dac2871fdee66ec22d7f3
Douglas Soares de Andrade <contato@douglasandrade.com>
2011-06-19 14:05:34 -0300

    Removing some unused imports (probably legacy code) and a pep8 fix

--- scikits/crab/datasets/base.py
+++ scikits/crab/datasets/base.py
@@ -3,25 +3,21 @@
 # License: BSD Style.
 
 import csv
-import shutil
-from os import environ
 from os.path import dirname
 from os.path import join
-from os.path import exists
-from os.path import expanduser
-from os.path import isdir
-from os import listdir
-from os import makedirs
 import numpy as np
 
 
 def load_movielens100k():
     module_path = dirname(__file__)
-    data_file = csv.reader(open(join(module_path, 'data', 'movielens100k.csv')))
+    data_file = csv.reader(open(join(module_path,
+                                     'data',
+                                     'movielens100k.csv')))
     temp = data_file.next()
     n_samples = int(temp[0])
     n_features = int(temp[1])
     data = np.empty((n_samples, n_features))
+
     for i, ir in enumerate(data_file):
         data[i] = np.asanyarray(ir, dtype=np.int)
     return data
