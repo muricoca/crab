c9bcdc486cdf17d9a880b6f13150d9c57dc37f93
Marcel Caraciolo <caraciol@gmail.com>
2011-07-31 02:35:58 -0300

    Added knn basic classes.

--- scikits/crab/recommenders/knn/classes.py
+++ scikits/crab/recommenders/knn/classes.py
@@ -1,7 +1,7 @@
 """
 Generalized Recommender models.
 
-This module contains basic recommender interfaces used throughout
+This module contains basic memory recommender interfaces used throughout
 the whole scikit-crab package.
 
 The interfaces are realized as abstract base classes (ie., some optional
@@ -13,12 +13,3 @@ can be subclassed).
 # Author: Marcel Caraciolo <marcel@muricoca.com>
 #
 # License: BSD Style.
-
-from ..base import BaseRecommender
-
-#===========================
-#Memory Based Recommender
-
-
-class MemoryRecommender(BaseRecommender):
-    pass
