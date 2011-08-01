bbac6564ae23e6d91371a46a1a63a1cef64f3ff3
Marcel Caraciolo <caraciol@gmail.com>
2011-08-01 01:51:13 -0300

    Added implementation for ItemsNeighborhoodStrategy

--- scikits/crab/recommenders/knn/item_strategies.py
+++ scikits/crab/recommenders/knn/item_strategies.py
@@ -15,4 +15,18 @@ from base import BaseCandidateItemsStrategy
 
 
 class ItemsNeighborhoodStrategy(BaseCandidateItemsStrategy):
-    pass
+    '''
+    Returns all items that have not been rated by the user and were
+    preferred by another user that has preferred at least one item that the
+    current has preferred too.
+    '''
+
+    def candidate_items(user_id, preferences_from_user, data_model, **params):
+        pref_item_ids = [user_id for user_id, score in preferences_from_user]
+        possible_items = []
+        for item_id in pref_item_ids:
+            item_preferences = data_model.preferences_for_item(item_id)
+            for user_id, score in item_preferences:
+                possible_items.append(data_model.items_from_user(user_id))
+
+        return possible_items - pref_item_ids
