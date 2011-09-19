4e051dfc7a455d33950a94544d9477bc8d45fcfb
Marcel Caraciolo <caraciol@gmail.com>
2011-09-19 08:27:21 -0300

    Added tests for evaluate_on_split for RMSE, NMAE and MAE.

--- scikits/crab/metrics/classes.py
+++ scikits/crab/metrics/classes.py
@@ -16,13 +16,13 @@ from metrics import root_mean_square_error
 from metrics import mean_absolute_error
 from metrics import normalized_mean_absolute_error
 from metrics import evaluation_error
-from metrics import precision_recall_fscore
+from cross_validation import KFold
 from metrics import precision_score
 from metrics import recall_score
 from metrics import f1_score
 from sampling import SplitSampling
 from scikits.learn.base import clone
-from ..models.utils import ItemNotFoundError
+from ..models.utils import ItemNotFoundError, UserNotFoundError
 
 
 #Collaborative Filtering Evaluator
@@ -61,6 +61,30 @@ def check_sampling(sampling, n):
         sampling = SplitSampling(n, evaluation_fraction=sampling)
 
     return sampling
+
+
+def check_cv(cv, n):
+    """Input checker utility for building a
+       cross validation in a user friendly way.
+
+   Parameters
+   ===========
+    sampling: an integer, a cv generator instance, or None
+        The input specifying which cv generator to use.
+        It can be an integer, in which case it is the number
+        of folds in a KFold, None, in which case 3 fold is used,
+        or another object, that will then be used as a cv generator.
+
+    n: an integer.
+        The number of elements.
+
+    """
+    if cv is None:
+        cv = 3
+    if operator.isNumberType(cv):
+        cv = KFold(n, cv, indices=True)
+
+    return cv
 
 
 class CfEvaluator(RecommenderEvaluator):
@@ -135,7 +159,6 @@ class CfEvaluator(RecommenderEvaluator):
 
         Examples
         --------
-
         """
         sampling_users = kwargs.pop('sampling_users', None)
         sampling_ratings = kwargs.pop('sampling_ratings', 0.7)
@@ -290,23 +313,147 @@ class CfEvaluator(RecommenderEvaluator):
             return {'mae': mae, 'nmae': nmae, 'rmse': rmse,
                     'precision': p, 'recall': r, 'f1score': f}
 
-    def evaluate_on_split(self, recommender, metric=None, **kwargs):
+    def evaluate_on_split(self, recommender, metric=None, cv=None, **kwargs):
         """
         Evaluate on the folds of a dataset split
 
         Parameters
         ----------
+        recommender: The BaseRecommender instance
+                The recommender instance to be evaluated.
+
         metric: [None|'rmse'|'f1score'|'precision'|'recall'|'nmae'|'mae']
-        If metrics is None, all metrics available will be evaluated.
+            If metrics is None, all metrics available will be evaluated.
         Otherwise it will return the specified metric evaluated.
+
+        sampling_users:  float or sampling, optional, default = None
+            If an float is passed, it is the percentage of evaluated
+        users. If sampling_users is None, all users are used in the
+        evaluation. Specific sampling objects can be passed, see
+        scikits.crab.metrics.sampling module for the list of possible
+        objects.
+
+        cv: integer or crossvalidation, optional, default = None
+            If an integer is passed, it is the number of fold (default 3).
+            Specific sampling objects can be passed, see
+            scikits.crab.metrics.cross_validation module for the list of
+            possible objects.
+
+        at: integer, optional, default = None
+            This number at is the 'at' value, as in 'precision at 5'.  For
+        example this would mean precision or recall evaluated by removing
+        the top 5 preferences for a user and then finding the percentage of
+        those 5 items included in the top 5 recommendations for that user.
+        If at is None, it will consider all the top 3 elements.
 
         Returns
         -------
-        Returns a score representing how well the recommender estimated the
-        preferences match real values.
+        score: dict
+            a dictionary containing the average results over
+            the different permutations on the split.
+
+        permutation_scores : array, shape = [n_permutations]
+            The scores obtained for each permutations.
 
         Examples
         --------
 
         """
-        raise NotImplementedError("cannot instantiate Abstract Base Class")
+        sampling_users = kwargs.pop('sampling_users', 0.7)
+        permutation = kwargs.pop('permutation', True)
+        at = kwargs.pop('at', 3)
+
+        if metric not in evaluation_metrics and metric is not None:
+            raise ValueError('metric %s is not recognized. valid keywords \
+              are %s' % (metric, evaluation_metrics.keys()))
+
+        permutation_scores = []
+        final_score = {'avg': {}, 'stdev': {}}
+
+        n_users = recommender.model.users_count()
+        sampling_users = check_sampling(sampling_users, n_users)
+        users_set, _ = sampling_users.split(permutation=permutation)
+
+        total_ratings = []
+        #Select the users to be evaluated.
+        user_ids = recommender.model.user_ids()
+        for user_id in user_ids[users_set]:
+            #Select the ratings to be evaluated.
+            preferences = recommender.model.preferences_from_user(user_id)
+            preferences = list(preferences)
+            total_ratings.extend([(user_id, preference)
+                                 for preference in preferences])
+
+        n_ratings = len(total_ratings)
+        cv = check_cv(cv, n_ratings)
+        #Defining the splits and run on the splits.
+        for train_set, test_set in cv:
+
+            training_set = {}
+            testing_set = {}
+
+            for idx in train_set:
+                user_id, pref = total_ratings[idx]
+                if recommender.model.has_preference_values():
+                    training_set.setdefault(user_id, {})
+                    training_set[user_id][pref[0]] = pref[1]
+                else:
+                    training_set.setdefault(user_id, {})
+                    training_set[user_id][pref] = 1.0
+
+            for idx in test_set:
+                user_id, pref = total_ratings[idx]
+                if recommender.model.has_preference_values():
+                    testing_set.setdefault(user_id, [])
+                    testing_set[user_id].append(pref)
+                else:
+                    testing_set.setdefault(user_id, [])
+                    testing_set[user_id].append((pref, 1.0))
+
+            #Evaluate the recommender.
+            recommender_training = self._build_recommender(training_set, \
+                                    recommender)
+
+            real_preferences = []
+            estimated_preferences = []
+
+            for user_id, preferences in testing_set.iteritems():
+                for item_id, preference in preferences:
+                    #Estimate the preferences
+                    try:
+                        estimated = recommender_training.estimate_preference(
+                                    user_id, item_id)
+                        real_preferences.append(preference)
+                    except:
+                        # It is possible that an item exists
+                        #in the test data but
+                        # not training data in which case
+                        #an exception will be
+                        # throw. Just ignore it and move on
+                        continue
+                    estimated_preferences.append(estimated)
+
+            #Return the error results.
+            if metric in ['rmse', 'mae', 'nmae']:
+                eval_function = evaluation_metrics[metric]
+                if metric == 'nmae':
+                    permutation_scores.append({
+                                metric: eval_function(real_preferences,
+                                                 estimated_preferences,
+                                recommender.model.maximum_preference_value(),
+                                recommender.model.minimum_preference_value())})
+                else:
+                    permutation_scores.append(
+                    {metric: eval_function(real_preferences,
+                                       estimated_preferences)})
+
+        #Compute the final score
+        for result in permutation_scores:
+            for key in result:
+                final_score['avg'].setdefault(key, [])
+                final_score['avg'][key].append(result[key])
+        for key in final_score['avg']:
+            final_score['stdev'][key] = np.std(final_score['avg'][key])
+            final_score['avg'][key] = np.average(final_score['avg'][key])
+
+        return permutation_scores, final_score
--- scikits/crab/metrics/tests/test_classes.py
+++ scikits/crab/metrics/tests/test_classes.py
@@ -113,3 +113,107 @@ def test_root_CfEvaluator_evaluate():
                     sampling_users=0.6, sampling_ratings=0.6)
     assert_true(nmae['nmae'] >= 0.0 and nmae['nmae'] <= 1.0)
 
+
+def test_root_CfEvaluator_evaluate_on_split():
+    """Check evaluate_on_split method in CfEvaluator """
+    evaluator = CfEvaluator()
+
+    #Test with invalid metric
+    assert_raises(ValueError, evaluator.evaluate_on_split, recsys, 'rank')
+
+    #Test with specified metric
+    rmse = evaluator.evaluate_on_split(recsys, 'rmse', permutation=False)
+    for p in rmse[0]:
+        assert_true(p['rmse'] >= 0.0 and p['rmse'] <= 1.0)
+    assert_true(rmse[1]['avg']['rmse'] >= 0.0 and rmse[1]['stdev']['rmse'] <= 1.0)
+
+    mae = evaluator.evaluate_on_split(recsys, 'mae', permutation=False)
+    for p in mae[0]:
+        assert_true(p['mae'] >= 0.0 and p['mae'] <= 1.0)
+    assert_true(mae[1]['avg']['mae'] >= 0.0 and mae[1]['stdev']['mae'] <= 1.0)
+
+    nmae = evaluator.evaluate_on_split(recsys, 'nmae', permutation=False)
+    for p in nmae[0]:
+        assert_true(p['nmae'] >= 0.0 and p['nmae'] <= 1.0)
+    assert_true(nmae[1]['avg']['nmae'] >= 0.0 and nmae[1]['stdev']['nmae'] <= 1.0)
+
+    #Test with boolean model
+    assert_raises(ValueError, evaluator.evaluate_on_split, boolean_recsys, 'rank')
+
+    #Test with specified metric
+    rmse = evaluator.evaluate_on_split(boolean_recsys, 'rmse', permutation=False)
+    for p in rmse[0]:
+        assert_true(p['rmse'] >= 0.0 and p['rmse'] <= 1.0)
+    assert_true(rmse[1]['avg']['rmse'] >= 0.0 and rmse[1]['stdev']['rmse'] <= 1.0)
+
+    mae = evaluator.evaluate_on_split(boolean_recsys, 'mae', permutation=False)
+    for p in mae[0]:
+        assert_true(p['mae'] >= 0.0 and p['mae'] <= 1.0)
+    assert_true(mae[1]['avg']['mae'] >= 0.0 and mae[1]['stdev']['mae'] <= 1.0)
+
+    nmae = evaluator.evaluate_on_split(boolean_recsys, 'nmae', permutation=False)
+    for p in nmae[0]:
+        assert_true(p['nmae'] >= 0.0 and p['nmae'] <= 1.0)
+    assert_true(nmae[1]['avg']['nmae'] >= 0.0 and nmae[1]['stdev']['nmae'] <= 1.0)
+
+
+    '''
+    precision = evaluator.evaluate(recsys, 'precision',
+                                permutation=False)
+    assert_true(precision['precision'] >= 0.0 and precision['precision'] <= 1.0)
+
+    recall = evaluator.evaluate(recsys, 'recall', permutation=False)
+    assert_true(recall['recall'] >= 0.0 and recall['recall'] <= 1.0)
+
+    f1score = evaluator.evaluate(recsys, 'f1score', permutation=False)
+    assert_true(f1score['f1score'] >= 0.0 and f1score['f1score'] <= 1.0)
+
+    all_scores = evaluator.evaluate(recsys, permutation=False)
+    assert_true(all_scores['f1score'] >= 0.0 and all_scores['f1score'] <= 1.0)
+    assert_true(all_scores['recall'] >= 0.0 and all_scores['recall'] <= 1.0)
+    assert_true(all_scores['precision'] >= 0.0 and all_scores['precision'] <= 1.0)
+    assert_true(all_scores['nmae'] >= 0.0 and all_scores['nmae'] <= 1.0)
+    assert_true(all_scores['mae'] >= 0.0 and all_scores['mae'] <= 1.0)
+    assert_true(all_scores['rmse'] >= 0.0 and all_scores['rmse'] <= 1.0)
+
+    #With values at sampling.
+    nmae = evaluator.evaluate(recsys, 'nmae', permutation=False,
+                    sampling_users=0.6, sampling_ratings=0.6)
+    assert_true(nmae['nmae'] >= 0.0 and nmae['nmae'] <= 1.0)
+
+    #Test with boolean recsys
+    assert_raises(ValueError, evaluator.evaluate, boolean_recsys, 'rank')
+
+    #Test with specified metric
+    rmse = evaluator.evaluate(boolean_recsys, 'rmse', permutation=False)
+    assert_true(rmse['rmse'] >= 0.0 and rmse['rmse'] <= 1.0)
+
+    mae = evaluator.evaluate(boolean_recsys, 'mae', permutation=False)
+    assert_true(mae['mae'] >= 0.0 and mae['mae'] <= 1.0)
+
+    nmae = evaluator.evaluate(boolean_recsys, 'nmae', permutation=False)
+    assert_true(nmae['nmae'] >= 0.0 and nmae['nmae'] <= 1.0)
+
+    precision = evaluator.evaluate(boolean_recsys, 'precision',
+                                permutation=False)
+    assert_true(precision['precision'] >= 0.0 and precision['precision'] <= 1.0)
+
+    recall = evaluator.evaluate(boolean_recsys, 'recall', permutation=False)
+    assert_true(recall['recall'] >= 0.0 and recall['recall'] <= 1.0)
+
+    f1score = evaluator.evaluate(boolean_recsys, 'f1score', permutation=False)
+    assert_true(f1score['f1score'] >= 0.0 and f1score['f1score'] <= 1.0)
+
+    all_scores = evaluator.evaluate(recsys, permutation=False)
+    assert_true(all_scores['f1score'] >= 0.0 and all_scores['f1score'] <= 1.0)
+    assert_true(all_scores['recall'] >= 0.0 and all_scores['recall'] <= 1.0)
+    assert_true(all_scores['precision'] >= 0.0 and all_scores['precision'] <= 1.0)
+    assert_true(all_scores['nmae'] >= 0.0 and all_scores['nmae'] <= 1.0)
+    assert_true(all_scores['mae'] >= 0.0 and all_scores['mae'] <= 1.0)
+    assert_true(all_scores['rmse'] >= 0.0 and all_scores['rmse'] <= 1.0)
+
+    #With values at sampling.
+    nmae = evaluator.evaluate(boolean_recsys, 'nmae', permutation=False,
+                    sampling_users=0.6, sampling_ratings=0.6)
+    assert_true(nmae['nmae'] >= 0.0 and nmae['nmae'] <= 1.0)
+    '''
